0510f0e0dd7881e7e902020257cd5e08b1b67f45
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2025-12-15 15:57:36 -0600

    chore: prettify `shellHook` and rm emulator boilerplate

--- template/flake.nix
+++ template/flake.nix
@@ -10,13 +10,6 @@
     };
     android-nixpkgs = {
       url = "github:tadfisher/android-nixpkgs";
-
-      # The main branch follows the "canary" channel of the Android SDK
-      # repository. Use another android-nixpkgs branch to explicitly
-      # track an SDK release channel.
-      #
-      # .../android-nixpkgs/stable, beta, preview, canary";
-
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -40,28 +33,11 @@
           function {
             pkgs = nixpkgs.legacyPackages.${system};
             android-sdk = android-nixpkgs.sdk.${system} (
-              sdkPkgs:
-              with sdkPkgs;
-              [
-                # Useful packages for building and testing.
+              sdkPkgs: with sdkPkgs; [
                 build-tools-34-0-0
                 cmdline-tools-13-0
                 platform-tools
                 platforms-android-30
-
-                # Other useful packages for a development environment.
-                # emulator
-                # ndk-26-1-10909125
-                # skiaparser-3
-                # sources-android-34
-              ]
-              ++ nixpkgs.lib.optionals (system == "x86_64-darwin" || system == "x86_64-linux") [
-                # system-images-android-29-google-apis-x86-64
-                # system-images-android-29-google-apis-playstore-x86-64
-              ]
-              ++ nixpkgs.lib.optionals (system == "aarch64-darwin") [
-                # system-images-android-29-google-apis-arm64-v8a
-                # system-images-android-29-google-apis-playstore-arm64-v8a
               ]
             );
           }
@@ -95,22 +71,17 @@
               fi
 
               echo -e "\nFTC Development Environment via Nix Flake\n"
-              echo -e "Build app:     ./gradlew assemble"
-              echo -e "Test app:      ./gradlew test"
-              echo -e "WiFi Connect:  adb connect 192.168.43.1:5555"
-              echo -e "Uninstall app: adb uninstall com.qualcomm.ftcrobotcontroller"
-              echo -e "Install app:   adb install ./TeamCode/build/outputs/apk/release/TeamCode-release.apk\n"
 
-              java --version && echo ""
-              adb --version && echo ""
+              echo -e "┌───────────────────────────────────────────────────────────────────────────────────────┐"
+              echo -e "│                                   Helpful Commands                                    │"
+              echo -e "├───────────────┬───────────────────────────────────────────────────────────────────────┤"
+              echo -e "│ Build app     │ ./gradlew assemble                                                    │"
+              echo -e "│ Test app      │ ./gradlew test                                                        │"
+              echo -e "│ WiFi Connect  │ adb connect 192.168.43.1:5555                                         │"
+              echo -e "│ Uninstall app │ adb uninstall com.qualcomm.ftcrobotcontroller                         │"
+              echo -e "│ Install app   │ adb install ./TeamCode/build/outputs/apk/release/TeamCode-release.apk │"
+              echo -e "└───────────────┴───────────────────────────────────────────────────────────────────────┘\n"
             '';
-
-            # prints emu. info and cmds if desired
-            # echo -e "Useful commands:\nAdd avd: avdmanager create avd -n test -k \"system-images;android-29;google_apis;x86_64\""
-            # echo -e "List emulators: emulator -list-avds"
-            # echo -e "Run emulator: emulator -avd test -no-metrics"
-            # echo -e "Run stateless emulator (allows for 2+ as well): postfix -read-only"
-            # emulator -version | sed '1d;2d;6d;7d;8d;9d;10d;11d;12d;13d;14d;'
           };
         }
       );
